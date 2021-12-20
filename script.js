637b545f72992de0e92e1de9e2ec1364bb9809ac
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-20 19:20:24 +0500

    program 15

--- script.js
+++ script.js
@@ -64,4 +64,25 @@ console.log(days_of_a_year(2015));
 console.log(days_of_a_year(2016));
 
 
+// Write a JavaScript function to get the quarter (1 to 4) of the year.   
+
+// Test Data :
+// console.log(quarter_of_the_year(new Date(2015, 1, 21)));
+// 2
+// console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+4function quarter_of_the_year(date) 
+  {
+    var month = date.getMonth() + 1;
+    return (Math.ceil(month / 3));
+  }
+
+
+console.log(quarter_of_the_year(new Date()));
+console.log(quarter_of_the_year(new Date(2015, 1, 21))); 
+console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+
+
+
 
