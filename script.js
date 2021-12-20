b9f6461fe7342521739b29d5e5f9149db2ddd42a
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-20 19:22:15 +0500

    program 16

--- script.js
+++ script.js
@@ -101,9 +101,25 @@ function days_passed(dt) {
   console.log(days_passed(new Date(2015, 11, 14)));
   
 
+//   Write a JavaScript function to convert a Unix timestamp to time.   
 
+//   Test Data :
+//   console.log(days_passed(new Date(2015, 0, 15)));
+//   15
+//   console.log(days_passed(new Date(2015, 11, 14)));
+//   348
 
  
+function Unix_timestamp(t)
+{
+var dt = new Date(t*1000);
+var hr = dt.getHours();
+var m = "0" + dt.getMinutes();
+var s = "0" + dt.getSeconds();
+return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
+}
+
+console.log(Unix_timestamp(1412743274));
 
 
 
