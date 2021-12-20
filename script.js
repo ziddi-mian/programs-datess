335e356eaf2033e711833ee0b5c759b3f650c402
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-20 19:18:12 +0500

    progra14

--- script.js
+++ script.js
@@ -40,5 +40,28 @@ function timeConvert(n) {
     }
     
     console.log(timeConvert(200));
+
     
+    //Write a JavaScript function to get the amount of days of a year.   
+
+// Test Data :
+// console.log(days_of_a_year(2015));
+// 365
+// console.log(days_of_a_year(2016));
+// 366
+
+function days_of_a_year(year) 
+{
+   
+  return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+console.log(days_of_a_year(2015));
+console.log(days_of_a_year(2016));
+
+
 
