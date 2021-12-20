3a3ffd0cf0c3500bfe0abeb9b3d5a7a79a7588b1
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-20 19:14:40 +0500

    program12

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// Write a JavaScript function to get the minimum date from an array of dates.   
+
+// Test Data :
+// console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+// Output :
+// "2015/01/03"
+
+function min_date(all_dates) {
+    var min_dt = all_dates[0],
+     min_dtObj = new Date(all_dates[0]);
+    all_dates.forEach(function(dt, index)
+     {
+     if ( new Date( dt ) < min_dtObj)
+     {
+     min_dt = dt;
+     min_dtObj = new Date(dt);
+     }
+     });
+    return min_dt;
+     }
+   console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+   
+   //Write a JavaScript function that will return the number of minutes in hours and minutes.   
+
+// Test Data :
+// console.log(timeConvert(200));
+// Output :
+// "200 minutes = 3 hour(s) and 20 minute(s)."
+
+
+function timeConvert(n) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+    }
+    
+    console.log(timeConvert(200));
+    
+
