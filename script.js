9cee32f2f39597140ad81472faa4c421413739d3
Muhammad Ahmad <88133500+ziddi-mian@users.noreply.github.com>
2021-12-23 09:52:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -120,6 +120,97 @@ return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);
 }
 
 console.log(Unix_timestamp(1412743274));
+// Write a JavaScript function to get 12-hour format of an hour with leading zeros.   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(hours_with_zeroes(dt));
+// "12"
+
+function hours_with_zeroes(dt) 
+{ 
+  return ((dt.getHours() % 12 || 12) < 10 ? '0' : '') + (dt.getHours() % 12 || 12);
+}
+
+dt = new Date(); 
+console.log(hours_with_zeroes(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(hours_with_zeroes(dt));
+
+
+// Write a JavaScript function to get 24-hour format of an hour without leading zeros.   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(hours_without_zeroes(dt));
+// 0]
+
+
+function hours_without_zeroes(dt) 
+{ 
+  return dt.getHours();
+}
+
+dt = new Date(); 
+console.log(hours_without_zeroes(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(hours_without_zeroes(dt));
+
+
+// Write a JavaScript function to get minutes with leading zeros (00 to 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(minutes_with_leading_zeros(dt));
+// "00"
+
+
+function minutes_with_leading_zeros(dt) 
+{ 
+  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+dt = new Date(); 
+console.log(minutes_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(minutes_with_leading_zeros(dt));
+
+
+// Write a JavaScript function to get seconds with leading zeros (00 through 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(seconds_with_leading_zeros(dt));
+// "00"
+
+function seconds_with_leading_zeros(dt) 
+{ 
+  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+dt = new Date(); 
+console.log(seconds_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(seconds_with_leading_zeros(dt));
+
+
+
+// Write a JavaScript function to get Timezone.   
+// Test Data :
+// dt = new Date();
+// console.log(seconds_with_leading_zeros(dt));
+// "India Standard Time"
+
+function seconds_with_leading_zeros(dt) 
+{ 
+  return /\((.*)\)/.exec(new Date().toString())[1];
+}
+
+dt = new Date(); 
+console.log(seconds_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(seconds_with_leading_zeros(dt));
 
 
 
