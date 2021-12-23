e04c14d52bb9d574e0911e21f659b95332612ca4
Muhammad Ahmad <88133500+ziddi-mian@users.noreply.github.com>
2021-12-23 09:52:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -212,5 +212,50 @@ console.log(seconds_with_leading_zeros(dt));
 dt = new Date(1989, 10, 1); 
 console.log(seconds_with_leading_zeros(dt));
 
+// Write a JavaScript function to get lowercase Ante meridiem and Post meridiem.
+
+function lower_am_pm(dt) 
+{ 
+   return dt.getHours() < 12 ? 'AM' : 'PM';
+}
+
+dt = new Date(); 
+console.log(lower_am_pm(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(lower_am_pm(dt));
+
+
+
+// 31. Write a JavaScript function to get uppercase Ante meridiem and Post meridiem.   function upper_am_pm(dt) 
+{ 
+    return dt.getHours() < 12 ? 'AM' : 'PM';
+  }
+  
+  dt = new Date(); 
+  console.log(upper_am_pm(dt)); 
+  
+  dt = new Date(1989, 10, 1); 
+  console.log(upper_am_pm(dt));
+
+  
+//   Write a JavaScript function to swatch Internet time (000 through 999).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(internet_time(dt));
+// 812
+
+function internet_time(dt) 
+{ 
+  return Math.floor((((dt.getUTCHours() + 1) % 24) + dt.getUTCMinutes() / 60 + dt.getUTCSeconds() / 3600) * 1000 / 24);
+}
+
+dt = new Date(); 
+console.log(internet_time(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(internet_time(dt));
+
+
 
 
